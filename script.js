04c9f3d239afcdd6c25ec9f3509357b519782afe
MindLated <ismyskyllz@gmail.com>
2024-08-09 07:51:23 +0200

    1.0.2

--- script.js
+++ script.js
@@ -22,3 +22,15 @@ document.getElementById('encryptButton').addEventListener('click', () => {
     const output = GameSaveSerializer.serialize(json);
     document.getElementById('output').value = output;
 });
+
+// Toggle dark mode
+const toggleThemeButton = document.getElementById('toggleTheme');
+toggleThemeButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    toggleThemeButton.innerHTML = isDarkMode ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
+});
+
+// Set initial icon based on the mode
+const isDarkMode = document.body.classList.contains('dark-mode');
+toggleThemeButton.innerHTML = isDarkMode ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
